fdca6cfb697979ed29242359252349bf94b52290
pb-totoro <jonesec2@gmail.com>
2020-04-07 20:43:21 -0400

    Set up savedAPI function placeholders

--- client/src/utils/savedAPI.js
+++ client/src/utils/savedAPI.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export default {
-   
+   getSaved: function() {
+      return axios.get("/api/saved");
+   },
+   saveBook: function(savedBook) {
+      return axios.post("/api/saved");
+   }
 }
